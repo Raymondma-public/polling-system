afa353121ec07a4c8a0498c15bd1b3240cbc9513
Raymondma-public <raymondma.public@gmail.com>
2018-10-21 16:07:22 +0800

    [Task] Add Read Me. Remove unused code

--- polling-system-client/src/app/app.component.ts
+++ polling-system-client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit,AfterViewInit } from '@angular/core';
-import {PollService} from './services/poll-service.service'
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { PollService } from './services/poll-service.service'
 import { Poll } from './models/poll';
 import { Chart } from "chart.js";
 
@@ -10,62 +10,52 @@ import { Chart } from "chart.js";
 })
 export class AppComponent implements OnInit {
   title = '';
-  total=80;
-  polls:Poll[];
+  total = 80;
+  polls: Poll[];
 
   chartOptions = {
-    responsive: true    
+    responsive: true
   }
 
-  labels =  ['YES', 'NO'];
+  labels = ['YES', 'NO'];
 
-  // STATIC DATA FOR THE CHART IN JSON FORMAT.
-  chartData = [
- 21, 59
-    
- 
-  ];
+  chartData = [21, 59];
 
-  // CHART COLOR.
-  // colors = [{backgroundColor: '#000'},{backgroundColor: '#333'}
-    
-  // ]
-
-  constructor(private pollService:PollService){
+  constructor(private pollService: PollService) {
 
   }
 
-  getPolls():void{
-    this.pollService.getAll().subscribe(res=>{
-      this.polls=res.polls;
-      this.polls.forEach(poll=>{
-        poll.publishedDate=new Date(poll.publishedDate*1000);
+  getPolls(): void {
+    this.pollService.getAll().subscribe(res => {
+      this.polls = res.polls;
+      this.polls.forEach(poll => {
+        poll.publishedDate = new Date(poll.publishedDate * 1000);
       })
       console.log(this.polls);
-      if(this.polls[0]){
+      if (this.polls[0]) {
         console.log(this.polls[0].publishedDate.getFullYear());
-        this.title=this.polls[0].title;
-      }else{
-       
+        this.title = this.polls[0].title;
+      } else {
+
       }
     }
-  );
+    );
   }
-  ngOnInit(){
+  ngOnInit() {
     this.getPolls();
   }
-  onChartClick(e,id) {
+  onChartClick(e, id) {
     console.log("id", id);
-    if(e.active.length > 0) {
+    if (e.active.length > 0) {
       console.log("Index", e.active[0]._index);
-      console.log("Data" , e.active[0]._chart.config.data.datasets[0].data[e.active[0]._index]);
-      console.log("Label" , e.active[0]._chart.config.data.labels[e.active[0]._index]);
-      this.pollService.postPollResponse(id,e.active[0]._chart.config.data.labels[e.active[0]._index]).subscribe(res=>{
-        if(res.message=='success'){
+      console.log("Data", e.active[0]._chart.config.data.datasets[0].data[e.active[0]._index]);
+      console.log("Label", e.active[0]._chart.config.data.labels[e.active[0]._index]);
+      this.pollService.postPollResponse(id, e.active[0]._chart.config.data.labels[e.active[0]._index]).subscribe(res => {
+        if (res.message == 'success') {
           this.getPolls();
         }
       });
-      }
+    }
 
   }
 
--- polling-system-client/src/app/services/poll-service.service.ts
+++ polling-system-client/src/app/services/poll-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { HttpClient ,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Poll } from '../models/poll';
 import { PollResponse } from '../models/poll-response';
 import { environment } from '../../environments/environment';
@@ -12,17 +12,17 @@ export class PollService {
 
   constructor(private http: HttpClient) { }
 
-  getAll():Observable<PollResponse>{
-      return this.http.get<PollResponse>(environment.getAPI("polls"));
+  getAll(): Observable<PollResponse> {
+    return this.http.get<PollResponse>(environment.getAPI("polls"));
   }
 
-  postPollResponse(pollId,optionLabel):Observable<any>{
-     const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded' );
+  postPollResponse(pollId, optionLabel): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
     //  const options = new RequestOptions({ headers: headers });
     const params = new URLSearchParams();
     params.append('id', pollId);
     params.append('option', optionLabel);
-    const p={id:pollId,option:optionLabel};
-    return this.http.post(environment.getAPI("polls"),{headers,params:p});
+    const p = { id: pollId, option: optionLabel };
+    return this.http.post(environment.getAPI("polls"), { headers, params: p });
   }
 }
